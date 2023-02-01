c392fe52577ba9e01fc043033a908b8b837cf1d6
samezzz <95895530+samezzz@users.noreply.github.com>
2023-02-01 00:03:13 +0000

    allowed host

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = [".vercel.app"]
+ALLOWED_HOSTS = ["localhost","127.0.0.1",".vercel.app"]
 
 
 # Application definition
